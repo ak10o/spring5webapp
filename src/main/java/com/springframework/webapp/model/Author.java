2e537b4ab346665cf0bbfbb35f526b02bd350a21
Avinash kumar <avinash369kumar@gmail.com>
2019-12-11 04:20:04 +0530

    overriden the equals and hashcode to follow best pratices
    set will be using this method to determine whether the object is duplicated or not.
    
    Current Implemenatation - if the id is same then the object is duplicated

--- src/main/java/com/springframework/webapp/model/Author.java
+++ src/main/java/com/springframework/webapp/model/Author.java
@@ -70,5 +70,34 @@ public class Author {
 	public void setId(Long id) {
 		this.id = id;
 	}
-	
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((firstName == null) ? 0 : firstName.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Author other = (Author) obj;
+		if (firstName == null) {
+			if (other.firstName != null)
+				return false;
+		} else if (!firstName.equals(other.firstName))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Author [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", books=" + books + "]";
+	}
 }
